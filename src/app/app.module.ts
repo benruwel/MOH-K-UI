e29a83917f549b958e6cc10aa52cc047d707aad3
Linda Tonui <tonuilinda@gmail.com>
2020-07-20 21:12:31 +0300

    Add the UI

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MapsComponent } from './maps/maps.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MapsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
